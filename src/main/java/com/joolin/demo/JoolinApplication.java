8a890688a2f6c19a09d4eeb96cf377e09a305b05
DarrenKimBat <uiucpass@gmail.com>
2020-06-25 19:37:48 +0900

    Initial commit

---
+++ src/main/java/com/joolin/demo/JoolinApplication.java
@@ -0,0 +1,13 @@
+package com.joolin.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoolinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JoolinApplication.class, args);
+    }
+
+}
