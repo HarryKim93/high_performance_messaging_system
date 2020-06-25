ea60da900ba236f7a17bda58b0df28e874878010
DarrenKimBat <uiucpass@gmail.com>
2020-06-25 23:39:38 +0900

    Configure of Kafka Settings.

--- src/main/java/com/joolin/demo/Controller/ChattingController.java
+++ src/main/java/com/joolin/demo/Controller/ChattingController.java
@@ -1,4 +1,49 @@
 package com.joolin.demo.Controller;
 
+import com.joolin.demo.Data.ChattingHistoryDAO;
+import com.joolin.demo.Receiver;
+import com.joolin.demo.Sender;
+import com.joolin.demo.model.ChattingMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
 public class ChattingController {
+    @Autowired
+    private Sender sender;
+
+    @Autowired
+    private Receiver receiver;
+
+    @Autowired
+    private ChattingHistoryDAO chattingHistoryDAO;
+
+    private static String BOOT_TOPIC="kafka-chatting";
+
+    @MessageMapping("/message")
+    @SendTo("/topic/public")
+    public void sendMessage(ChattingMessage message) throws Exception{
+        message.setTimeStamp(System.currentTimeMillis());
+        chattingHistoryDAO.save(message);
+        sender.send(BOOT_TOPIC, message);
+    }
+
+    @RequestMapping("/history")
+    public List<ChattingMessage> getChattingHistory() throws Exception{
+        System.out.println("History:\n");
+        return chattingHistoryDAO.get();
+    }
+
+    @MessageMapping("/file")
+    @SendTo("/topic/chatting")
+    public ChattingMessage sendFile(ChattingMessage message) throws Exception{
+        return new ChattingMessage(message.getFileName(), message.getRawData(), message.getUser());
+    }
 }
